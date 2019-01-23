edb59818f9dd19b5756d6a7b7cc1220bf842ca4e
Emiton Alves <emiton.alves@fmglobal.com>
2019-01-23 14:54:30 -0500

    Now able to sign in to main dashboard

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -17,19 +17,58 @@ namespace WebScraper
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            string test = "This is ";
-            string test2 = "a test";
-            Console.WriteLine(test + test2);
+            Console.WriteLine("*****STARTING*****");
 
             // Create instance of web scraper
             using (IWebDriver webDriver = new ChromeDriver())
             {
                 webDriver.Navigate().GoToUrl("https://finance.yahoo.com");
 
-                webDriver.FindElement(By.Id("uh-signedin")).Click();
+                //webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                //webDriver.FindElement(By.Id("uh-signedin")).Click();
+
+                try
+                {
+                    webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                    webDriver.FindElement(By.Id("uh-signedin")).Click();
+                }
+                catch (OpenQA.Selenium.NoSuchElementException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    // throw new OpenQA.Selenium.NoSuchElementException("ID not found");
+                }
+
+
+                try
+                {
+                    webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                    IWebElement email = webDriver.FindElement(By.Id("login-username"));
+                    email.SendKeys("financetester321@gmail.com");
+                    webDriver.FindElement(By.XPath("//*[@id=\"login-signin\"]")).Click();
+                    Console.WriteLine("GOT THE EMAIL BABAYYYY!");
+                }
+                catch (OpenQA.Selenium.NoSuchElementException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                try
+                {
+                    webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                    IWebElement password = webDriver.FindElement(By.Id("login-passwd"));
+                    password.SendKeys("thisisnew1234");
+                    webDriver.FindElement(By.Id("login-signin")).Click();
+                    Console.WriteLine("WE LOGGED IN!");
+                }
+                catch (OpenQA.Selenium.NoSuchElementException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
             }
+
+
 
         }
 
