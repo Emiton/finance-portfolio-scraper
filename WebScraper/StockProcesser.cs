7dca40df3b8659b43a0e6f40252767d5ac26b0e9
Emiton Alves <emiton_alves@my.uri.edu>
2019-01-29 12:21:02 -0500

    StockProcessor will process all stock information before adding to database

---
+++ WebScraper/StockProcesser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace WebScraper
+{
+    class StockProcesser
+    {
+        public List<StockObject> ParseStockTable(IList<IWebElement> stocks)
+        {
+            List<StockObject> parsedStocks = new List<StockObject>();
+            int numberOfStocks = stocks.Count;
+            Console.WriteLine(numberOfStocks);
+            return parsedStocks;
+        }
+    }
+}
